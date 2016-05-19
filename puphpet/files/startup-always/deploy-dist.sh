5aecfa27a10dd799b7149d952d5532656c4e0860
Jesse LaVere <jesse.lavere@gmail.com>
2016-05-19 13:03:52 -0700

    Add VM and configure.

---
+++ puphpet/files/startup-always/deploy-dist.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# deploy vagrant version of dist files
+echo '**************************************************************************'
+echo 'Setup your .dist deployment in puphpet/files/startup-always/deploy-dist.sh'
+echo '**************************************************************************'
+# cp /vagrant/documentation/vagrant_dist/local.php /vagrant/config/autoload/local.php
